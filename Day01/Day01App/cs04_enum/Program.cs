0a83f8d88d9e80bdab6c37d8418291c19c26e074
kooweajeeI <leejw2873@naver.com>
2023-04-04 17:11:11 +0900

    4월 4일
    C# 시작

---
+++ Day01/Day01App/cs04_enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs04_enum
+{
+    internal class Program
+    {
+        enum HomeTown
+        {
+            SEOUL = 1,
+            DAEJEON = 2,
+            DAEGU = 4,
+            BUSAN = 6,
+            JEJU = 9
+        };
+        static void Main(string[] args)
+        {
+            HomeTown myHomeTown;
+
+            myHomeTown = HomeTown.BUSAN;
+
+            if (myHomeTown == HomeTown.SEOUL)
+            {
+                Console.WriteLine("서울 출신이군요!");
+            }
+            else if (myHomeTown == HomeTown.DAEJEON)
+            {
+                Console.WriteLine("충청도에유~");
+            }
+            else if (myHomeTown == HomeTown.DAEGU) {
+                Console.WriteLine("대구여~");
+            }
+            else if (myHomeTown == HomeTown.BUSAN)
+            {
+                Console.WriteLine("부산입니데이~");
+            }
+        }
+    }
+}
---
+++ Day01/Day01App/cs09_nullcondition/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs09_nullcondition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Foo myfoo = null;
+            /*
+            int? bar;
+            if( myfoo != null)
+            {
+                bar = myfoo.member;
+            }
+            else
+            {
+                bar = null;
+            }*/
+            // 위의 주석부분을 모두 축약시킴
+            int? bar = myfoo?.member;
+        }
+    }
+
+    class Foo
+    {
+        public int member;
+    }
+}
